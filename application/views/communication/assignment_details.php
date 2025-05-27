3f4b8459b3dcebf6505b1ad4d561e63e557a9021
pranjalmittal01 <pranjalmittal810@gmail.com>
2025-05-27 15:54:56 +0530

    fix bug

--- application/views/communication/assignment_details.php
+++ application/views/communication/assignment_details.php
@@ -25,10 +25,6 @@
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <!-- Then load Select2 -->
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-
-
 </head>
 
 <body class="skin-blue sidebar-mini sidebar-collapse">
